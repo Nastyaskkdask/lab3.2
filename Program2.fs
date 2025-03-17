35d238dd1f0525a6a9c90b23dfae469347e76e4e
Nastyaskkdask <144699427+Nastyaskkdask@users.noreply.github.com>
2025-03-18 02:50:43 +0500

    Add files via upload

---
+++ Program2.fs
@@ -0,0 +1,32 @@
+﻿open System
+
+let find (stringSeq: seq<string>) : string option =
+    let shortest (a:string) (b:string) : string =
+        printfn "Сравнение: '%s' vs '%s'" a b
+        if a.Length <= b.Length then a
+        else b
+
+    stringSeq
+    |> Seq.fold (fun acc str ->
+        match acc with
+        | None -> Some str
+        | Some currentShortest -> Some (shortest currentShortest str)) None
+
+
+let mySeq : seq<string> =
+    printf "Введите строки (каждую на новой строке, пустая строка - конец ввода):\n"
+    let rec readLines acc =
+        match Console.ReadLine() with
+        | "" -> acc
+        | line -> readLines (Seq.append acc (seq { yield line }))
+
+    readLines Seq.empty
+
+printfn "Сгенерированный список строк:"
+mySeq |> Seq.iter (printfn "%s")
+
+match find mySeq with
+| Some shortestString -> printfn "\nСамая короткая строка: %s" shortestString
+| None -> printfn "\nСписок пуст."
+
+
